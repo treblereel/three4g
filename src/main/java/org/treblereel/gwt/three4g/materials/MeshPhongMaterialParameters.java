05007ac19717ab47781e3d75324d07ef42844795
Dmitrii Tikhomirov <dtikhomi@redhat.com>
2018-03-12 16:19:25 +0100

    MeshPhongMaterial

---
+++ src/main/java/org/treblereel/gwt/three4g/materials/MeshPhongMaterialParameters.java
@@ -0,0 +1,13 @@
+package org.treblereel.gwt.three4g.materials;
+
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+/**
+ * @author Dmitrii Tikhomirov <chani@me.com>
+ * Created by treblereel on 3/12/18.
+ */
+@JsType(isNative = true, namespace= JsPackage.GLOBAL, name="Object")
+public class MeshPhongMaterialParameters extends MaterialParameters{
+
+}
