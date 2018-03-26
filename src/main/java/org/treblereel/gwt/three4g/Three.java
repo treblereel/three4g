637dbec5915d45e87be5d9b327d51c5fa0356692
Dmitrii Tikhomirov <dtikhomi@redhat.com>
2018-03-26 19:41:27 +0200

    ThreeJSLoaderOnLoad

--- src/main/java/org/treblereel/gwt/three4g/Three.java
+++ src/main/java/org/treblereel/gwt/three4g/Three.java
@@ -3,33 +3,34 @@ package org.treblereel.gwt.three4g;
 import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.ScriptInjector;
+import org.treblereel.gwt.three4g.js.ThreeJsTextResource;
 
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Dmitrii Tikhomirov <chani@me.com>
  * Created by treblereel on 3/26/18.
  */
 public class Three implements EntryPoint {
-    /**
-     * The entry point method, called automatically by loading a module that
-     * declares an implementing class as an entry point.
-     */
+
     @Override
     public void onModuleLoad() {
-        java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Three.class.getSimpleName());
-
-/*        ScriptInjector.fromUrl("js/three.js").setCallback(new Callback<Void, Exception>() {
-            @Override
-            public void onFailure(Exception reason) {
-                logger.log(Level.WARNING," unable to load three.js " + reason.getMessage());
-            }
-
-            @Override
-            public void onSuccess(Void result) {
-                logger.log(Level.SEVERE," three.js loaded successfully");
-
-            }
-        }).inject();*/
+        loadThreeJS();
     }
+
+    public static void loadThreeJS(){
+        load("three.js");
+    }
+
+    public static void loadThreeJSMin(){
+        load("three.min.js");
+    }
+
+    private static void load(String scriptName) {
+        ScriptInjector.fromString(ThreeJsTextResource.IMPL.getThreeJS().getText())
+                .setWindow(ScriptInjector.TOP_WINDOW).inject();
+
+    }
+
 }
--- src/main/java/org/treblereel/gwt/three4g/js/ThreeJsTextResource.java
+++ src/main/java/org/treblereel/gwt/three4g/js/ThreeJsTextResource.java
@@ -1,8 +1,21 @@
 package org.treblereel.gwt.three4g.js;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
 /**
  * @author Dmitrii Tikhomirov <chani@me.com>
  * Created by treblereel on 3/26/18.
  */
-public class ThreeJsTextResource {
+public interface ThreeJsTextResource extends ClientBundle{
+
+    ThreeJsTextResource IMPL = GWT.create(ThreeJsTextResource.class);
+
+
+    @Source("js/three.js")
+    TextResource getThreeJS();
+
+    @Source("js/three.min.js")
+    TextResource getThreeJSMin();
 }
