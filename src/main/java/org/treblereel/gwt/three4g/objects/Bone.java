a7f6fe9b5e4bc427d8e7dd1b9a3ca5a34819b4e5
Dmitrii Tikhomirov <dtikhomi@redhat.com>
2018-04-30 14:21:24 +0200

    Bone

---
+++ src/main/java/org/treblereel/gwt/three4g/objects/Bone.java
@@ -0,0 +1,33 @@
+package org.treblereel.gwt.three4g.objects;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsType;
+import org.treblereel.gwt.three4g.core.Object3D;
+
+/**
+ * A bone which is part of a Skeleton. The skeleton in turn is used by the SkinnedMesh. Bones are almost identical to a blank Object3D.
+ *
+ * @author Dmitrii Tikhomirov <chani@me.com>
+ * Created by treblereel on 4/30/18.
+ */
+@JsType(isNative = true, namespace = "THREE")
+public class Bone extends Object3D {
+
+    /**
+     * Used to check whether this or derived classes are bones. Default is true.
+     * You should not change this, as it used internally for optimisation.
+     */
+    public boolean isBone;
+
+    /**
+     * Set to 'Bone', this can be used to find all Bones in a scene.
+     */
+    public String type;
+
+    @JsConstructor
+    public Bone() {
+
+    }
+
+
+}
