bcffaed273a8806ab534671b47412d2698bd5783
Dmitrii Tikhomirov <dtikhomi@redhat.com>
2018-05-02 17:57:42 +0200

    CircleGeometry and CircleBufferGeometry

---
+++ src/main/java/org/treblereel/gwt/three4g/geometries/CircleBufferGeometry.java
@@ -0,0 +1,46 @@
+package org.treblereel.gwt.three4g.geometries;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsType;
+import org.treblereel.gwt.three4g.core.Geometry;
+
+/**
+ * This is the BufferGeometry port of CircleGeometry.
+ *
+ * @author Dmitrii Tikhomirov <chani@me.com>
+ * Created by treblereel on 5/2/18.
+ */
+@JsType(isNative = true, namespace = "THREE")
+public class CircleBufferGeometry extends Geometry {
+
+    /**
+     * @param radius      — Radius of the circle, default = 1.
+     * @param segments    — Number of segments (triangles), minimum = 3, default = 8.
+     */
+    @JsConstructor
+    public CircleBufferGeometry(float radius, float segments) {
+
+    }
+
+    /**
+     * @param radius      — Radius of the circle, default = 1.
+     * @param segments    — Number of segments (triangles), minimum = 3, default = 8.
+     * @param thetaStart  — Start angle for first segment, default = 0 (three o'clock position).
+     */
+    @JsConstructor
+    public CircleBufferGeometry(float radius, float segments, float thetaStart) {
+
+    }
+
+    /**
+     * @param radius      — Radius of the circle, default = 1.
+     * @param segments    — Number of segments (triangles), minimum = 3, default = 8.
+     * @param thetaStart  — Start angle for first segment, default = 0 (three o'clock position).
+     * @param thetaLength — The central angle, often called theta, of the circular sector. The default is 2*Pi, which makes for a complete circle.
+     */
+    @JsConstructor
+    public CircleBufferGeometry(float radius, float segments, float thetaStart, float thetaLength) {
+
+    }
+
+}
---
+++ src/main/java/org/treblereel/gwt/three4g/geometries/CircleGeometry.java
@@ -0,0 +1,48 @@
+package org.treblereel.gwt.three4g.geometries;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsType;
+import org.treblereel.gwt.three4g.core.Geometry;
+
+/**
+ * CircleGeometry is a simple shape of Euclidean geometry. It is contructed from a number of triangular segments that are
+ * oriented around a central point and extend as far out as a given radius. It is built counter-clockwise from a start
+ * angle and a given central angle. It can also be used to create regular polygons, where the number of segments determines
+ * the number of sides.
+ *
+ * @author Dmitrii Tikhomirov <chani@me.com>
+ * Created by treblereel on 5/2/18.
+ */
+@JsType(isNative = true, namespace = "THREE")
+public class CircleGeometry extends Geometry {
+
+    /**
+     * @param radius      — Radius of the circle, default = 1.
+     * @param segments    — Number of segments (triangles), minimum = 3, default = 8.
+     */
+    @JsConstructor
+    public CircleGeometry(float radius, float segments) {
+
+    }
+
+    /**
+     * @param radius      — Radius of the circle, default = 1.
+     * @param segments    — Number of segments (triangles), minimum = 3, default = 8.
+     * @param thetaStart  — Start angle for first segment, default = 0 (three o'clock position).
+     */
+    @JsConstructor
+    public CircleGeometry(float radius, float segments, float thetaStart) {
+
+    }
+
+    /**
+     * @param radius      — Radius of the circle, default = 1.
+     * @param segments    — Number of segments (triangles), minimum = 3, default = 8.
+     * @param thetaStart  — Start angle for first segment, default = 0 (three o'clock position).
+     * @param thetaLength — The central angle, often called theta, of the circular sector. The default is 2*Pi, which makes for a complete circle.
+     */
+    @JsConstructor
+    public CircleGeometry(float radius, float segments, float thetaStart, float thetaLength) {
+
+    }
+}
