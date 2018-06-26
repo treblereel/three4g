0bf509bf0fa59022b5fd87641d6afcffd25ae9bd
Dmitrii Tikhomirov <dtikhomi@redhat.com>
2018-06-26 22:00:12 +0200

    better Uniforms support

--- src/main/java/org/treblereel/gwt/three4g/materials/parameters/Uniforms.java
+++ src/main/java/org/treblereel/gwt/three4g/materials/parameters/Uniforms.java
@@ -1,33 +1,33 @@
 package org.treblereel.gwt.three4g.materials.parameters;
 
-import elemental2.core.JsObject;
 import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 import jsinterop.base.Js;
 import jsinterop.base.JsPropertyMap;
+import org.treblereel.gwt.three4g.core.JsObject;
+
 
 /**
  * @author Dmitrii Tikhomirov
  * Created by treblereel on 6/7/18.
  */
 @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
-public class Uniforms extends JsObject {
+public class Uniforms {
 
     @JsOverlay
-    public final Uniforms addToUniforms(String name, Object value) {
-        JsPropertyMap holder =  Js.uncheckedCast(this);
-
+    public final Uniforms set(String name, Object value) {
+        JsObject holder = Js.uncheckedCast(this);
         JsPropertyMap temp = JsPropertyMap.of();
         temp.set("value", value);
-        holder.set(name, temp);
+        holder.setProperty(name, temp);
         return this;
     }
 
     @JsOverlay
-    public final JsObject get(String name) {
+    public final <T> T get(String name) {
         JsPropertyMap instance = Js.uncheckedCast(this);
-        return (JsObject) instance.get(name);
+        return (T) ((JsObject) instance.get(name)).getProperty("value");
     }
 
 }
